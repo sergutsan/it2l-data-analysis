ca404095051fa8aebf1475979c30f42bb667a53c
Sergio Gutierrez-Santos (Sergut) <sergut@gmail.com>
2015-06-11 17:49:46 +0100

    Modified script to that they are still usable in command line.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
-javac -cp .:gson-2.2.4.jar:poi-3.12-20150511.jar:mysql-connector-java-5.1.35-bin.jar MysqlToXls.java
-rm -rf mysql/MysqlToXls.class
-mv MysqlToXls.class mysql
+rm -rf src/db/MysqlToXls.class src/db/ConnectionData.class
+javac -cp .:gson-2.2.4.jar:poi-3.12-20150511.jar:mysql-connector-java-5.1.35-bin.jar src/db/MysqlToXls.java
+rm -rf bin
+mkdir -p bin/db
+mv src/db/MysqlToXls.class src/db/ConnectionData.class bin/db
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-java -cp .:gson-2.2.4.jar:poi-3.12-20150511.jar:mysql-connector-java-5.1.35-bin.jar mysql.MysqlToXls
+cd bin/
+java -cp .:../gson-2.2.4.jar:../poi-3.12-20150511.jar:../mysql-connector-java-5.1.35-bin.jar db.MysqlToXls
+cd ..
 
